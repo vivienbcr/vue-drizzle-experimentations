14bc946ee2ff432553c7acadbb297bcf3392a155
vivien <vivienbcr@protonmail.com>
2020-03-25 12:07:19 +0100

    init

---
+++ app/src/drizzleOptions.js
@@ -0,0 +1,42 @@
+import Web3 from "web3";
+
+import HelloWorld from "./contracts/HelloWorld.json";
+
+// const options = {
+//   web3: {
+//     block: false,
+//     fallback: {
+//       type: 'ws',
+//       url: 'ws://127.0.0.1:9545'
+//     }
+//   },
+
+//   // The contracts to monitor
+//   contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+//   events: {
+//     // monitor SimpleStorage.StorageSet events
+//     SimpleStorage: ['StorageSet']
+//   },
+//   polls: {
+//     // check accounts ever 15 seconds
+//     accounts: 15000
+//   }
+// }
+const options = {
+    web3: {
+      block: false,
+      customProvider: new Web3("ws://localhost:8545"),
+      networkWhitelist: [
+        1, // Mainnet
+        3, // Ropsten
+        4, // Rinkeby
+        5, // Goerli
+        42 // Kovan
+      ]
+    },
+    contracts: [HelloWorld],
+    events: {
+      SimpleStorage: ["StorageSet"]
+    }
+  };
+export default options
