0e73615191414fa2db9350dc245d695649a158d3
vivien <vivien.boucher@outlook.com>
2020-03-28 13:06:59 +0100

    mapgetters

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,21 +1,43 @@
 <template>
   <div class="home">
+    <v-container>
+      {{ activeAccount }}
+      <!-- TODO: V-if drizzle initialized -->
+      <v-col class="d-flex" cols="12" sm="6">
+        <!-- <v-select :items="items" label="Outlined style" outlined></v-select> -->
+        <v-btn @click="changeAccount">Click</v-btn>
+      </v-col>
+    </v-container>
+    <!-- {{ activeAccount }}
     <drizzle-account />
-     <drizzle-contract contractName="fightGame" method="GetAllPlayers" />
-     <drizzle-contract-form contractName="fightGame" method="CreatePlayer" :methodArgs="{from: activeAccount, gas: 3000000}" />
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <drizzle-contract contractName="fightGame" method="GetAllPlayers" />
+    <drizzle-contract-form
+      contractName="fightGame"
+      method="CreatePlayer"
+      :methodArgs="req"
+    /> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from "@/components/HelloWorld.vue";
-
+import { mapGetters } from "vuex";
 export default {
   name: "Home",
-  components: {
-    // HelloWorld
+  components: {},
+  computed: {
+    // mix the getters into computed with object spread operator
+    ...mapGetters({
+      activeAccount: "accounts/activeAccount"
+    })
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {
+    changeAccount() {
+      console.log("hello");
+    }
   }
 };
 </script>
